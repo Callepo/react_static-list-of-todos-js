e859aacf9a492d85f233d2c8dd778d05a1befafd
Callepo <clifford.agata@hotmail.com>
2025-07-20 20:12:36 +0200

    task solutions

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,9 @@
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.length > 0 &&
+      todos.map(task => <TodoInfo key={task.id} todo={task} />)}
+  </section>
+);
