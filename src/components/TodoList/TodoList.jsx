cf30f22031e0c8e20ca9c939c39b8bc78d093ba9
Callepo <clifford.agata@hotmail.com>
2025-07-20 20:18:11 +0200

    Recommended Changes

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,7 +3,8 @@ import { TodoInfo } from '../TodoInfo/TodoInfo';
 // Add the required props
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
-    {todos.length > 0 &&
-      todos.map(task => <TodoInfo key={task.id} todo={task} />)}
+    {todos.map(task => (
+      <TodoInfo key={task.id} todo={task} />
+    ))}
   </section>
 );
